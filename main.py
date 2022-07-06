65b65931f4ba537c129ce1d7831f47d23107ac24
Michaelzhouisnotwhite <zyqing601@163.com>
2022-07-06 21:23:33 +0800

    add terminal args

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from argparse import ArgumentParser
+
+
+def args():
+    parser = ArgumentParser()
+    parser.add_argument("-p", "--port", required=False, default=8080, type=int, help="The port that the proxy server listen on.")
+    return parser.parse_args()
+
+
 def main():
     from server import ProxyServer
-    ProxyServer(8080).start()
+    argv = args()
+    ProxyServer(port=argv.port).start()
 
 
 if __name__ == '__main__':
